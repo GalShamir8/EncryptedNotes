abb273c930f598cdf58ef38c2ae79d0d295d1619
Galshamir8 <galshamir8@gmail.com>
2023-02-04 16:55:39 +0200

    Refactor EncryptedSharedPreference

--- app/src/main/java/com/example/encryptednotes/shared/EncryptedSharedPreference.java
+++ app/src/main/java/com/example/encryptednotes/shared/EncryptedSharedPreference.java
@@ -4,64 +4,82 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.util.Base64;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.security.GeneralSecurityException;
-import java.security.KeyStore;
+import com.example.encryptednotes.models.Note;
+
+import java.security.Key;
+import java.security.MessageDigest;
 import java.util.ArrayList;
+import java.util.HashMap;
+
+import java.util.Map.Entry;
 
 import javax.crypto.Cipher;
-import javax.crypto.CipherInputStream;
-import javax.crypto.CipherOutputStream;
+import javax.crypto.spec.SecretKeySpec;
 
 public class EncryptedSharedPreference {
 
-    private static final String TRANSFORMATION = "AES/CBC/PKCS5Padding";
-    private static final String KEY_ALIAS = "notes_key";
-
+    private static final String ALGORITHM = "AES";
+    private static final String PREFERENCE_NAME = "encrypted_notes_pref";
+    private static final String KEY_NAME = "encryption_key";
     private SharedPreferences mSharedPreferences;
-    private KeyStore mKeyStore;
+    private Cipher mCipher;
 
-    public EncryptedSharedPreference(Context context) throws GeneralSecurityException, IOException {
-        mKeyStore = KeyStore.getInstance("AndroidKeyStore");
-        mKeyStore.load(null);
-        mSharedPreferences = context.getSharedPreferences("notes_prefs", Context.MODE_PRIVATE);
+    public EncryptedSharedPreference(Context context) throws Exception {
+        mSharedPreferences = context.getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE);
+        mCipher = Cipher.getInstance(ALGORITHM);
     }
 
-    public void putString(String key, String value) throws GeneralSecurityException, IOException {
-        KeyStore.PrivateKeyEntry privateKeyEntry = (KeyStore.PrivateKeyEntry) mKeyStore.getEntry(KEY_ALIAS, null);
-        Cipher inCipher = Cipher.getInstance(TRANSFORMATION);
-        inCipher.init(Cipher.ENCRYPT_MODE, privateKeyEntry.getCertificate().getPublicKey());
-
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        CipherOutputStream cipherOutputStream = new CipherOutputStream(outputStream, inCipher);
-        cipherOutputStream.write(value.getBytes("UTF-8"));
-        cipherOutputStream.close();
-
-        mSharedPreferences.edit().putString(key, Base64.encodeToString(outputStream.toByteArray(), Base64.DEFAULT)).apply();
+    private Key generateKey() throws Exception {
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] keyBytes = digest.digest(KEY_NAME.getBytes("UTF-8"));
+        return new SecretKeySpec(keyBytes, ALGORITHM);
     }
 
-    public String getString(String key) throws GeneralSecurityException, IOException {
-        KeyStore.PrivateKeyEntry privateKeyEntry = (KeyStore.PrivateKeyEntry) mKeyStore.getEntry(KEY_ALIAS, null);
-        Cipher outCipher = Cipher.getInstance(TRANSFORMATION);
-        outCipher.init(Cipher.DECRYPT_MODE, privateKeyEntry.getPrivateKey());
+    public void putNote(String key, String value){
+        try {
+            mCipher.init(Cipher.ENCRYPT_MODE, generateKey());
+            // Encrypt the value
+            byte[] encryptedValue = mCipher.doFinal(value.getBytes());
+            String encodedValue = Base64.encodeToString(encryptedValue, Base64.DEFAULT);
 
-        byte[] encryptedData = Base64.decode(mSharedPreferences.getString(key, ""), Base64.DEFAULT);
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(encryptedData);
-        CipherInputStream cipherInputStream = new CipherInputStream(inputStream, outCipher);
-        ArrayList<Byte> values = new ArrayList<>();
-        int nextByte;
-        while ((nextByte = cipherInputStream.read()) != -1) {
-            values.add((byte) nextByte);
+            // Save the encrypted value in the shared preference
+            SharedPreferences.Editor editor = mSharedPreferences.edit();
+            editor.putString(key, encodedValue);
+            editor.apply();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getString(String key) {
+        try {
+            mCipher.init(Cipher.DECRYPT_MODE, generateKey());
+            // Get the encrypted value from the shared preference
+            String encodedValue = mSharedPreferences.getString(key, null);
+
+            // Decrypt the value
+            byte[] encryptedValue = Base64.decode(encodedValue, Base64.DEFAULT);
+            byte[] decryptedValue = mCipher.doFinal(encryptedValue);
+
+            return new String(decryptedValue);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-        byte[] bytes = new byte[values.size()];
-        for (int i = 0; i < bytes.length; i++) {
-            bytes[i] = values.get(i);
-        }
+        return null;
+    }
 
-        return new String(bytes, 0, bytes.length, "UTF-8");
+    public ArrayList<Note> getAlNotes() {
+        ArrayList<Note> notes = new ArrayList<>();
+        HashMap<String, String> data = (HashMap<String, String>) mSharedPreferences.getAll();
+        for (Entry<String, String> entry: data.entrySet()){
+            notes.add(buildNote(entry.getKey(), entry.getValue()));
+        }
+        return notes;
+    }
+
+    private Note buildNote(String header, String content) {
+        return new Note(header, getString(header));
     }
 }
 
