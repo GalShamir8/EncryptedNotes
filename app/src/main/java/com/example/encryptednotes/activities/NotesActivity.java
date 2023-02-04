6110c9c03185595670cf91803ab1886b5bd4e693
Galshamir8 <galshamir8@gmail.com>
2023-02-04 17:10:37 +0200

    Add basic notes activity, will be extended

---
+++ app/src/main/java/com/example/encryptednotes/activities/NotesActivity.java
@@ -0,0 +1,63 @@
+package com.example.encryptednotes.activities;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.encryptednotes.R;
+import com.example.encryptednotes.models.Note;
+import com.example.encryptednotes.shared.EncryptedSharedPreference;
+
+import java.util.ArrayList;
+
+public class NotesActivity extends AppCompatActivity {
+
+
+    private EditText mNoteEditText;
+    private EditText mNoteHeaderEditText;
+    private Button mSaveButton;
+    private EncryptedSharedPreference mEncryptedSharedPreference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notes);
+        initView();
+        setListeners();
+
+        try {
+            mEncryptedSharedPreference = new EncryptedSharedPreference(this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        loadNotes();
+    }
+
+    private void loadNotes() {
+        ArrayList<Note> notes = mEncryptedSharedPreference.getAlNotes();
+    }
+
+    private void setListeners() {
+        mSaveButton.setOnClickListener(e -> saveNote());
+    }
+
+    private void saveNote() {
+        String header = mNoteHeaderEditText.getText().toString();
+        String note = mNoteEditText.getText().toString();
+        if (header.trim().isEmpty()) {
+            Toast.makeText(this, "Please enter a header for the note", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        mEncryptedSharedPreference.putNote(header, note);
+        Toast.makeText(this, "Note saved", Toast.LENGTH_SHORT).show();
+    }
+
+    private void initView() {
+        mNoteEditText = findViewById(R.id.note_edit_text);
+        mNoteHeaderEditText = findViewById(R.id.note_header_edit_text);
+        mSaveButton = findViewById(R.id.save_button);
+    }
+}
